d4e0b40a0bbb8c162e73dfd25ee019e10c0b412b
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-02-22 17:36:10 +0100

    add a message handler to the demo project

---
+++ LamdaHandlers.Demo/Handler.cs
@@ -0,0 +1,15 @@
+﻿namespace LamdaHandlers.Demo
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    class Handler : IHandleMessages<MyMessage>
+    {
+        public Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            Console.WriteLine("Hello from an ordinary message handler");
+            return Task.CompletedTask;
+        }
+    }
+}
